4fd51611cd5b782b64c4a29f7fc81acf38ad2171
Vinicius Recoaro <vrecoaro@hotmail.com>
2024-10-09 19:24:45 -0300

    HomeFragment - Finished chart legend

--- app/src/main/java/br/vino/flightcontrol/ui/home/HomeFragment.kt
+++ app/src/main/java/br/vino/flightcontrol/ui/home/HomeFragment.kt
@@ -15,6 +15,7 @@ import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.formatter.PercentFormatter
+import java.text.NumberFormat
 
 class HomeFragment : Fragment() {
 
@@ -39,6 +40,140 @@ class HomeFragment : Fragment() {
     private fun setupListeners(){
         viewModel.flightsPerStatusList.observe(viewLifecycleOwner){ flightsList ->
             initExpensePerCategoryChart(flightsList)
+            val nOfCategories = flightsList.size
+            val colors = viewModel.getPieChartCategoriesColors()
+            when (nOfCategories) {
+                1 -> {
+                    //Visibility
+                    binding.ivIconFlightsLegend1.visibility = View.VISIBLE
+                    binding.tvTextFlightsList1.visibility = View.VISIBLE
+                    binding.ivIconFlightsLegend2.visibility = View.GONE
+                    binding.tvTextFlightsList2.visibility = View.GONE
+                    binding.ivIconFlightsLegend3.visibility = View.GONE
+                    binding.tvTextFlightsList3.visibility = View.GONE
+                    binding.ivIconFlightsLegend4.visibility = View.GONE
+                    binding.tvTextFlightsList4.visibility = View.GONE
+                    binding.ivIconFlightsLegend5.visibility = View.GONE
+                    binding.tvTextFlightsList5.visibility = View.GONE
+
+                    //Colors
+                    binding.ivIconFlightsLegend1.setColorFilter(colors[0])
+
+                    //Text
+                    val text1 = "${flightsList[0].second} Voos:\n${flightsList[0].first}"
+                    binding.tvTextFlightsList1.text = text1
+                }
+                2 -> {
+                    //Visibility
+                    binding.ivIconFlightsLegend1.visibility = View.VISIBLE
+                    binding.tvTextFlightsList1.visibility = View.VISIBLE
+                    binding.ivIconFlightsLegend2.visibility = View.VISIBLE
+                    binding.tvTextFlightsList2.visibility = View.VISIBLE
+                    binding.ivIconFlightsLegend3.visibility = View.GONE
+                    binding.tvTextFlightsList3.visibility = View.GONE
+                    binding.ivIconFlightsLegend4.visibility = View.GONE
+                    binding.tvTextFlightsList4.visibility = View.GONE
+                    binding.ivIconFlightsLegend5.visibility = View.GONE
+                    binding.tvTextFlightsList5.visibility = View.GONE
+
+                    //Colors
+                    binding.ivIconFlightsLegend1.setColorFilter(colors[0])
+                    binding.ivIconFlightsLegend2.setColorFilter(colors[1])
+
+                    //Text
+                    val text1 = "${flightsList[0].second} Voos:\n${flightsList[0].first}"
+                    binding.tvTextFlightsList1.text = text1
+                    val text2 = "${flightsList[1].second} Voos:\n${flightsList[1].first}"
+                    binding.tvTextFlightsList2.text = text2
+                }
+                3 -> {
+                    //Visibility
+                    binding.ivIconFlightsLegend1.visibility = View.VISIBLE
+                    binding.tvTextFlightsList1.visibility = View.VISIBLE
+                    binding.ivIconFlightsLegend2.visibility = View.VISIBLE
+                    binding.tvTextFlightsList2.visibility = View.VISIBLE
+                    binding.ivIconFlightsLegend3.visibility = View.VISIBLE
+                    binding.tvTextFlightsList3.visibility = View.VISIBLE
+                    binding.ivIconFlightsLegend4.visibility = View.GONE
+                    binding.tvTextFlightsList4.visibility = View.GONE
+                    binding.ivIconFlightsLegend5.visibility = View.GONE
+                    binding.tvTextFlightsList5.visibility = View.GONE
+
+                    //Colors
+                    binding.ivIconFlightsLegend1.setColorFilter(colors[0])
+                    binding.ivIconFlightsLegend2.setColorFilter(colors[1])
+                    binding.ivIconFlightsLegend3.setColorFilter(colors[2])
+
+                    //Text
+                    val text1 = "${flightsList[0].second} Voos:\n${flightsList[0].first}"
+                    binding.tvTextFlightsList1.text = text1
+                    val text2 = "${flightsList[1].second} Voos:\n${flightsList[1].first}"
+                    binding.tvTextFlightsList2.text = text2
+                    val text3 = "${flightsList[2].second} Voos:\n${flightsList[2].first}"
+                    binding.tvTextFlightsList3.text = text3
+                }
+                4 -> {
+                    //Visibility
+                    binding.ivIconFlightsLegend1.visibility = View.VISIBLE
+                    binding.tvTextFlightsList1.visibility = View.VISIBLE
+                    binding.ivIconFlightsLegend2.visibility = View.VISIBLE
+                    binding.tvTextFlightsList2.visibility = View.VISIBLE
+                    binding.ivIconFlightsLegend3.visibility = View.VISIBLE
+                    binding.tvTextFlightsList3.visibility = View.VISIBLE
+                    binding.ivIconFlightsLegend4.visibility = View.VISIBLE
+                    binding.tvTextFlightsList4.visibility = View.VISIBLE
+                    binding.ivIconFlightsLegend5.visibility = View.GONE
+                    binding.tvTextFlightsList5.visibility = View.GONE
+
+                    //Colors
+                    binding.ivIconFlightsLegend1.setColorFilter(colors[0])
+                    binding.ivIconFlightsLegend2.setColorFilter(colors[1])
+                    binding.ivIconFlightsLegend3.setColorFilter(colors[2])
+                    binding.ivIconFlightsLegend4.setColorFilter(colors[3])
+
+                    //Text
+                    val text1 = "${flightsList[0].second} Voos:\n${flightsList[0].first}"
+                    binding.tvTextFlightsList1.text = text1
+                    val text2 = "${flightsList[1].second} Voos:\n${flightsList[1].first}"
+                    binding.tvTextFlightsList2.text = text2
+                    val text3 = "${flightsList[2].second} Voos:\n${flightsList[2].first}"
+                    binding.tvTextFlightsList3.text = text3
+                    val text4 = "${flightsList[3].second} Voos:\n${flightsList[3].first}"
+                    binding.tvTextFlightsList4.text = text4
+                }
+                5 -> {
+                    //Visibility
+                    binding.ivIconFlightsLegend1.visibility = View.VISIBLE
+                    binding.tvTextFlightsList1.visibility = View.VISIBLE
+                    binding.ivIconFlightsLegend2.visibility = View.VISIBLE
+                    binding.tvTextFlightsList2.visibility = View.VISIBLE
+                    binding.ivIconFlightsLegend3.visibility = View.VISIBLE
+                    binding.tvTextFlightsList3.visibility = View.VISIBLE
+                    binding.ivIconFlightsLegend4.visibility = View.VISIBLE
+                    binding.tvTextFlightsList4.visibility = View.VISIBLE
+                    binding.ivIconFlightsLegend5.visibility = View.VISIBLE
+                    binding.tvTextFlightsList5.visibility = View.VISIBLE
+
+                    //Colors
+                    binding.ivIconFlightsLegend1.setColorFilter(colors[0])
+                    binding.ivIconFlightsLegend2.setColorFilter(colors[1])
+                    binding.ivIconFlightsLegend3.setColorFilter(colors[2])
+                    binding.ivIconFlightsLegend4.setColorFilter(colors[3])
+                    binding.ivIconFlightsLegend5.setColorFilter(colors[4])
+
+                    //Text
+                    val text1 = "${flightsList[0].second} Voos:\n${flightsList[0].first}"
+                    binding.tvTextFlightsList1.text = text1
+                    val text2 = "${flightsList[1].second} Voos:\n${flightsList[1].first}"
+                    binding.tvTextFlightsList2.text = text2
+                    val text3 = "${flightsList[2].second} Voos:\n${flightsList[2].first}"
+                    binding.tvTextFlightsList3.text = text3
+                    val text4 = "${flightsList[3].second} Voos:\n${flightsList[3].first}"
+                    binding.tvTextFlightsList4.text = text4
+                    val text5 = "${flightsList[4].second} Voos:\n${flightsList[4].first}"
+                    binding.tvTextFlightsList5.text = text5
+                }
+            }
         }
     }
 

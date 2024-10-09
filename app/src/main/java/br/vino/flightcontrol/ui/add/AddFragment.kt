84d89340fec9e482ea548cbbc2311d9d7971c0b8
Vinicius Recoaro <vrecoaro@hotmail.com>
2024-10-09 19:49:51 -0300

    FragmentAdd - Finished layout

--- app/src/main/java/br/vino/flightcontrol/ui/add/AddFragment.kt
+++ app/src/main/java/br/vino/flightcontrol/ui/add/AddFragment.kt
@@ -12,9 +12,6 @@ import br.vino.flightcontrol.databinding.FragmentAddBinding
 class AddFragment : Fragment() {
 
     private var _binding: FragmentAddBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,16 +19,11 @@ class AddFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val addViewModel =
-            ViewModelProvider(this).get(AddViewModel::class.java)
-
         _binding = FragmentAddBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        addViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
+
         return root
     }
 
